5f3da9fec733b15e3adb7dba467fd5a85a26a7d3
𝐡𝐚𝐢𝐝𝐞𝐫𝐒𝐘 <haider13ahmed2@gmail.com>
2025-12-19 18:10:35 +0300

    Update main.c

--- source/main.c
+++ source/main.c
@@ -1,13 +1,17 @@
-#include <ps4.h>
+#include "ps4.h"
 
 int _main(struct thread *td, void *uap) {
     initKernel();
     initLibc();
     
     char msg[100];
-    sprintf(msg, "Hello World! \n haider is here");
+    for(int i=0; i<100; i++) msg[i] = 0;
+    
+    char* text = "Hello World! \n Gemini is here.";
+    for(int i=0; text[i] != '\0'; i++) msg[i] = text[i];
 
     sceSysUtilSendSystemNotificationWithText(222, msg);
 
     return 0;
 }
+
